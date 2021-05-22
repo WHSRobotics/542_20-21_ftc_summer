722147a6200a5c719f056fe5fc3e431751768851
Perry <perryhan0123@gmail.com>
2021-05-22 15:29:37 -0700

    Copied robot utilities

---
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/firstOpMode.java
@@ -0,0 +1,16 @@
+package org.whitneyrobotics.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class firstOpMode extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("I'm alive", true);
+    }
+
+    @Override
+    public void loop() {
+        //telemetry.addLine;
+    }
+}
